a092d4c455832b8eaa701795c4f5aa0496421b06
ben <ben.loh.15@ucl.ac.uk>
2018-07-05 14:07:00 +0100

    Finish small model template

--- applications/run_3d_pose.py
+++ applications/run_3d_pose.py
@@ -15,29 +15,32 @@ from pose_3d.data_helpers import *
 
 
 if __name__ == '__main__':
-    pm_3d = PoseModel3d(os.path.join(
-                            os.path.dirname(os.path.realpath(__file__)),
-                        'logs'))
-    basename = 'ung_143_26'
-    data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 
-                            basename)
-    maps_files = glob.glob(os.path.join(data_dir, 
-                                        basename + '_c*_maps.mat'))
-    info_files = glob.glob(os.path.join(data_dir, 
-                                        basename + '_c*_info.mat'))
-    frames_paths = glob.glob(os.path.join(data_dir, 
-                                        basename + '_c*_frames'))
-    dataset = tf.data.Dataset.from_tensor_slices(
-        (maps_files, info_files, frames_paths))
+    basenames = ['ung_143_26']  # Use os.listdir() in future
+    dataset_dir = os.path.dirname(os.path.realpath(__file__))  # path to all data folders
+    maps_files = []
+    info_files = []
+    frames_paths = []
+    for basename in basenames:
+        data_dir = os.path.join(dataset_dir, basename)
+        maps_files.append(glob.glob(os.path.join(data_dir,
+                                                 basename + '_c*_maps.mat')))
+        info_files.append(glob.glob(os.path.join(data_dir, 
+                                                 basename + '_c*_info.mat')))
+        frames_paths.append(glob.glob(os.path.join(data_dir,
+                                                   basename + '_c*_frames')))
 
-    dataset = dataset.flat_map(lambda mf, pf, fp: 
-        tf.data.Dataset.from_tensor_slices(
-            tuple(tf.py_func(read_maps_poses_images, [mf, pf, fp], 
-                             [tf.float32, tf.float32, tf.float32]))))
+    pm_3d = PoseModel3d((None, 120, 160, 19),
+                        72)
+
     with pm_3d.graph.as_default():
-        iterator = dataset.make_one_shot_iterator()
-        next_element = iterator.get_next()
-        print(pm_3d.sess.run(next_element))
+        dataset = tf.data.Dataset.from_tensor_slices(
+            (maps_files, info_files, frames_paths))
+
+        dataset = dataset.flat_map(lambda mf, pf, fp: 
+            tf.data.Dataset.from_tensor_slices(
+                tuple(tf.py_func(read_maps_poses_images, [mf, pf, fp], 
+                                [tf.float32, tf.float32, tf.float32]))))
+    pm_3d.train(dataset, epochs=5, batch_size=2)
 
     # in_array = np.array([1, 2, 3])[np.newaxis]
     # pm_3d.train(in_array, np.array([2, 4, 6])[np.newaxis])
--- deps/pose_3d/pose_model_3d.py
+++ deps/pose_3d/pose_model_3d.py
@@ -4,16 +4,37 @@
 import tensorflow as tf
 
 class PoseModel3d:
-    def __init__(self, summary_dir):
+    def __init__(self, input_shape, n_outputs: int,
+                 summary_dir='/tmp/tensorflow_logs',
+                 saver_path='/tmp/tensorflow_ckpts/3dpose.ckpt',
+                 restore_model=True):
         self.graph = tf.Graph()
         with self.graph.as_default():
             config = tf.ConfigProto(device_count={'CPU' : 1, 'GPU' : 0})
             self.sess = tf.Session(config=config)
-            self.inputs = tf.placeholder(tf.float32, shape=[None, 3])
-            self.outputs = _linear_layer('linear1', self.inputs)
+
+            self.inputs = tf.placeholder(tf.float32, shape=input_shape)
+            self.outputs = _build_model(self.inputs, n_outputs)
+
             self.sess.run(tf.global_variables_initializer())
+
+            self.saver_path = saver_path
+            self.saver = tf.train.Saver()
+            if restore_model:
+                try:
+                    self.saver.restore(self.sess, self.saver_path)
+                except tf.errors.InvalidArgumentError:
+                    print("No model checkpoint found for given path {}"
+                          .format(self.saver_path))
+                    print("Continuing without loading model")
+
             self.writer = tf.summary.FileWriter(summary_dir)
             self.writer.add_graph(tf.get_default_graph())
+
+    def save_model(self, save_model_path: str):
+        tf.saved_model.simple_save(self.sess, save_model_path,
+                                   {'model_in': self.inputs},
+                                   {'model_out': self.outputs})
 
     def estimate(self, input_inst):
         with self.graph.as_default():
@@ -21,24 +42,46 @@ class PoseModel3d:
                 self.sess.run(self.outputs,
                               feed_dict={self.inputs: input_inst})
             return out
-    
-    def train(self, x, y_true):
+
+    def train(self, dataset, epochs: int, batch_size: int):
         with self.graph.as_default():
+            dataset = dataset.batch(batch_size)
+            iterator = dataset.make_initializable_iterator()
+            heatmaps, gt_pose, _ = iterator.get_next()
+
             loss = tf.losses.mean_squared_error(
-                labels=y_true, predictions=self.outputs)
+                    labels=gt_pose, predictions=self.outputs)
             tf.summary.scalar('loss', loss)
 
             merged_summary = tf.summary.merge_all()
-            
+
             optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
             train = optimizer.minimize(loss)
-            for i in range(20):
-                _, summary = self.sess.run((train, merged_summary),
-                                             feed_dict={self.inputs: x})
-                self.writer.add_summary(summary, i)
-    
 
-def _linear_layer(var_scope: str, inputs):
-    with tf.variable_scope(var_scope):
-        linear = tf.layers.dense(inputs, 3)
-    return linear
+            for i in range(epochs):
+                self.sess.run(iterator.initializer)
+                while True:
+                    try:
+                        next_heatmap = self.sess.run(heatmaps)
+                        _, summary = self.sess.run((train, merged_summary),
+                                                    feed_dict={
+                                                        self.inputs: next_heatmap})
+                        self.writer.add_summary(summary, i)
+                    except tf.errors.OutOfRangeError:
+                        break
+                if i % 10 == 0:
+                    self.saver.save(self.sess, self.saver_path)
+
+
+def _build_model(inputs, n_outputs: int):
+    with tf.variable_scope('conv1'):
+        conv1 = \
+            tf.layers.conv2d(inputs, n_outputs, [3, 3])
+        relu1 = tf.nn.relu(conv1)
+    with tf.variable_scope('flatten1'):
+        flatten1 = \
+            tf.layers.flatten(relu1)
+    with tf.variable_scope('linear1'):
+        linear1 = tf.layers.dense(flatten1, n_outputs)
+        relu2 = tf.nn.relu(linear1)
+    return relu2
