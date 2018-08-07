Potentially fix the global rotation to not give NaN

applications/check_data.py
@@ -16,7 +16,7 @@ SUMMARY_DIR = '/home/ben/tensorflow_logs/3d_pose'
 SAVER_PATH = '/home/ben/tensorflow_logs/3d_pose/ckpts/3d_pose.ckpt'
 
 START_NAME = '01_01'  # Modify with name to start at
-CHECK_HEATMAPS = False
+CHECK_HEATMAPS = True
 CHECK_JOINTS = True
 
 
@@ -26,7 +26,7 @@ def read_maps(maps_file):
     heatmaps = np.transpose(maps_dict['heat_mat'], (3, 0, 1, 2))
         # to shape: time, height, width, n_joints = 19
 
-    return heatmaps, maps_file
+    return heatmaps
 
 def read_joints(info_file):
     print(info_file, file=sys.stderr)
@@ -36,7 +36,7 @@ def read_joints(info_file):
     joints2d = info_dict['joints2D']
     pose = info_dict['pose']
 
-    return joints2d.astype(np.float32), info_file, pose
+    return joints2d.astype(np.float32), pose
 
 
 if __name__ == '__main__':
@@ -66,18 +66,15 @@ if __name__ == '__main__':
             if CHECK_HEATMAPS:
                 value = read_maps(maps_file)
                 shape = value[0].shape
-                filename = value[1]
                 assert shape[1] == 240
             if CHECK_JOINTS:
                 value = read_joints(info_file)
                 shape = value[0].shape
-                info_filename = value[1]
-                filename = info_filename[:-9] + '_maps.mat'
                 assert len(shape) == 3
                 assert shape[0] == 2
                 assert shape[1] == 24
-                assert (np.amax(value[2]) < 2 * np.pi)
-                assert (np.amin(value[2]) > -2 * np.pi)
+                assert (np.amax(value[1]) < 2 * np.pi)
+                assert (np.amin(value[1]) > -2 * np.pi)
         except AssertionError:
-            print(filename)
+            print(maps_file)
             continue

applications/remove_paf.py
@@ -15,13 +15,15 @@ DATASET_PATH = '/mnt/Data/ben/surreal/SURREAL/data/cmu/train/run0/'
 SUMMARY_DIR = '/home/ben/tensorflow_logs/3d_pose'
 SAVER_PATH = '/home/ben/tensorflow_logs/3d_pose/ckpts/3d_pose.ckpt'
 
-START_NAME = '62_22'  # Modify with name to start at
+START_NAME = '01_01'  # Modify with name to start at
 
 
 def remove_paf_from_maps(maps_file):
     print(maps_file, file=sys.stderr)
     maps_dict = scipy.io.loadmat(maps_file)
     assert 'heat_mat' in maps_dict
+    heatmaps = np.transpose(maps_dict['heat_mat'], (3, 0, 1, 2))
+    assert heatmaps.shape[1] == 240
     assert 'detected_2D' in maps_dict
     assert 'visibility_2D' in maps_dict
     try:

deps/pose_3d/network.py
@@ -13,6 +13,7 @@ def build_model(inputs, inputs_locs, training: bool):
         with tf.variable_scope('init_conv'):
             in_channels = inputs.get_shape().as_list()[-1]
 
+            inputs = tf.check_numerics(inputs, "in_heatmaps_not_finite")
             init_conv1 = tf.layers.conv2d(inputs, in_channels, [3, 3])
             bn1 = tf.layers.batch_normalization(init_conv1, training=training)
             conv_relu1 = tf.nn.relu(bn1)
@@ -21,6 +22,7 @@ def build_model(inputs, inputs_locs, training: bool):
             mn = _mobilenetv2(conv_relu1, training, alpha=1.4)
 
         with tf.variable_scope('init_dense'):
+            inputs_locs = tf.check_numerics(inputs_locs, "in_locs_not_finite")
             features_flat = tf.layers.flatten(mn)
             locations_flat = tf.layers.flatten(inputs_locs)
             in_concat = tf.concat([features_flat, locations_flat], axis=1)
@@ -39,8 +41,8 @@ def build_model(inputs, inputs_locs, training: bool):
 
         with tf.variable_scope('out_fc'):
             out = tf.layers.dense(bl_relu, 79) # 24*3 rotations + 7 cam params
-
-        tf.summary.histogram('out_pose', out[:, :72])
+            
+        # tf.summary.histogram('out_pose', out[:, :72])
 
     return out
 

deps/pose_3d/pose_model_3d.py
@@ -3,8 +3,8 @@ import pkg_resources
 
 import tensorflow as tf
 from tensorflow import keras
-import numpy as np
 from tensorboard.plugins.beholder import Beholder
+import numpy as np
 
 from .network import build_model, build_discriminator
 from . import config
@@ -159,15 +159,15 @@ class PoseModel3d:
 
             out_pose_norm = tf.norm(tf.reshape(out_pose, [-1, 3]), axis=1)
             out_pose_norm_zeros = tf.zeros(tf.shape(out_pose_norm))
-            greater_mask = tf.greater(out_pose_norm, config.reg_joint_limit)
             out_pose_too_large = tf.where(
-                greater_mask, out_pose_norm, out_pose_norm_zeros)
+                tf.greater(out_pose_norm, config.reg_joint_limit),
+                out_pose_norm, out_pose_norm_zeros)
             reg_loss = tf.losses.mean_squared_error(
                 labels=out_pose_norm_zeros, predictions=out_pose_too_large,
                 weights=config.reg_loss_scale)
             tf.summary.scalar('reg_loss', reg_loss, family='losses')
 
-            total_loss = pose_loss + reg_loss  # + pose_loss_direct
+            total_loss = pose_loss + reg_loss
 
             if self.mesh_loss or self.reproject_loss:
                 # TODO: 3D mesh loss uses ground truth global rotation
@@ -225,13 +225,20 @@ class PoseModel3d:
                     predictions=out_2d_gt_pose, delta=self.img_side_len/15,
                     weights=config.cam_loss_scale)
                 tf.summary.scalar('cam_loss', cam_loss, family='losses')
-                total_loss += cam_loss
+                # camera axis y-component should point up and focal length > 0
+                cam_limits = tf.gather(self.outputs, [76, 78], axis=1)
+                cam_zeros = tf.zeros(tf.shape(cam_limits))
+                cam_not_positive = tf.where(
+                    tf.less(cam_limits, cam_zeros), cam_limits, cam_zeros)
+                cam_loss_not_positive = tf.losses.mean_squared_error(
+                    labels=cam_zeros, predictions=cam_not_positive)
+                total_loss += cam_loss + cam_loss_not_positive
 
                 with tf.variable_scope("render"):
                     render = utils.render_mesh_verts_cam(
                         gt_meshes, 
                         self.outputs[:, 72:75], self.outputs[:, 75:78],
-                        tf.atan(1.0 / self.outputs[:, 78]) * 360 / np.pi, 
+                        tf.atan2(1.0, self.outputs[:, 78]) * 360 / np.pi,
                         self.mesh_faces)
                     # lights = tf.constant([[-2., 0., 0.], [0., -2., 0.],
                     #                       [0., 0., -2.], [-1., -1., -1.]])
@@ -280,7 +287,8 @@ class PoseModel3d:
             train_handle = self.sess.run(iterator.string_handle())
 
             _, _, gt_pose, betas, smpl_joints2d = self.next_input
-            gt_pose = utils.rotate_global_pose(gt_pose)
+            # with tf.variable_scope("rotate_global"):
+            #     gt_pose = utils.rotate_global_pose(gt_pose)
             out_pose = self.outputs[:, :72]
 
             total_loss = self.get_encoder_losses(
@@ -289,7 +297,7 @@ class PoseModel3d:
             if self.discriminator:
                 disc_total_loss, disc_enc_loss = self.get_discriminator_loss(
                     out_pose, gt_pose)
-                disc_optimizer = tf.train.AdamOptimizer(learning_rate=0.0002)
+                disc_optimizer = tf.train.AdamOptimizer(learning_rate=4e-4)
                 discriminator_vars = tf.get_collection(
                     tf.GraphKeys.TRAINABLE_VARIABLES, scope='discriminator')
                 train_discriminator = disc_optimizer.minimize(
@@ -301,7 +309,7 @@ class PoseModel3d:
             tf.summary.scalar('total_loss', total_loss, family='losses')
             summary = tf.summary.merge_all()
 
-            optimizer = tf.train.AdamOptimizer(learning_rate=0.0001)
+            optimizer = tf.train.AdamOptimizer(learning_rate=2e-4)
             encoder_vars = tf.get_collection(
                 tf.GraphKeys.TRAINABLE_VARIABLES, scope='encoder')
             update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)

deps/pose_3d/utils.py
@@ -63,29 +63,62 @@ def rotate_global_pose(thetas):
     # is aligned with the z axis. Make it so the person's vertical is aligned
     # with the y axis.
     batch_size = tf.shape(thetas)[0]
-    global_rot = thetas[:, :3]
-    global_rot_mat = project.rodrigues_batch(global_rot)
-    # turn_x = np.array([[1.0,  0.0, 0.0],
-    #                    [0.0,  0.0, 1.0],
-    #                    [0.0, -1.0, 0.0]])
+
+    turn_x = tf.constant([np.pi / 2, 0.0, 0.0])
+    turn_x_batch = tf.reshape(tf.tile(turn_x, [batch_size]), [batch_size, 3])
+
+    global_rot_vec = add_axis_angle_rotations(turn_x_batch, thetas[:, :3])
+
+    # global_rot = thetas[:, :3]
+    # global_rot_mat = project.rodrigues_batch(global_rot)
+    # turn_x = tf.constant([[1.0,  0.0, 0.0],
+    #                       [0.0,  0.0, 1.0],
+    #                       [0.0, -1.0, 0.0]])
     # turn_y = tf.constant([[0.0, 0.0, -1.0],
     #                       [0.0, 1.0,  0.0],
     #                       [1.0, 0.0,  0.0]])
-    turn_b = tf.constant([[0.0, 1.0, 0.0],
-                          [0.0, 0.0, 1.0],
-                          [1.0, 0.0, 0.0]])
-    turn_b = tf.reshape(tf.tile(turn_b, [batch_size, 1]), [batch_size, 3, 3])
-    global_rot_mat = tf.matmul(turn_b, global_rot_mat)
+    # turn_both = tf.constant([[0.0, 1.0, 0.0],
+    #                       [0.0, 0.0, 1.0],
+    #                       [1.0, 0.0, 0.0]])
+    # turn_b = tf.reshape(tf.tile(turn_b, [batch_size, 1]), [batch_size, 3, 3])
+    # global_rot_mat = tf.matmul(turn_b, global_rot_mat)
 
+    # Rotation matrix to axis-angle
     # https://en.wikipedia.org/wiki/Rotation_matrix#Determining_the_axis
-    global_rv_1 = global_rot_mat[:, 2, 1] - global_rot_mat[:, 1, 2]
-    global_rv_2 = global_rot_mat[:, 0, 2] - global_rot_mat[:, 2, 0]
-    global_rv_3 = global_rot_mat[:, 1, 0] - global_rot_mat[:, 0, 1]
+    # Note the case where rotation angle = pi (R = Rt) is treated as angle = 0
+    # global_rv_1 = global_rot_mat[:, 2, 1] - global_rot_mat[:, 1, 2]
+    # global_rv_2 = global_rot_mat[:, 0, 2] - global_rot_mat[:, 2, 0]
+    # global_rv_3 = global_rot_mat[:, 1, 0] - global_rot_mat[:, 0, 1]
+    # global_rot_vec = tf.stack([global_rv_1, global_rv_2, global_rv_3], axis=1)
+    # global_rot_vec_norm = tf.norm(global_rot_vec, axis=1, keepdims=True)
+    # is_zero = tf.equal(tf.squeeze(global_rot_vec_norm), 0.0)
+    # global_rot_vec = global_rot_vec / global_rot_vec_norm
+    # global_rot_vec = tf.where(is_zero,
+    #                           tf.zeros_like(global_rot_vec), global_rot_vec)
 
-    global_rot_vec = tf.stack([global_rv_1, global_rv_2, global_rv_3], axis=1)
-    global_rot_vec = tf.nn.l2_normalize(global_rot_vec, axis=1)
-    global_rot_vec = global_rot_vec * tf.acos(
-        (tf.trace(global_rot_mat) - 1) / 2)[:, tf.newaxis]
+    # global_rot_vec = global_rot_vec * tf.asin(global_rot_vec_norm / 2)
+
     thetas = tf.concat([global_rot_vec, thetas[:, 3:]], axis=1)
-
     return thetas
+
+
+def add_axis_angle_rotations(rv1, rv2):
+    # given angle*axis rotation vectors a*l and b*m, result angle*axis: c*n
+    # cos(c/2) = cos(a/2)cos(b/2) - sin(a/2)sin(b/2) (l . m)
+    # sin(c/2) (n) = 
+    #   sin(a/2)cos(b/2) (l) + cos(a/2)sin(b/2) (m) + sin(a/2)sin(b/2) (l x m)
+    a = tf.norm(rv1, axis=1)
+    b = tf.norm(rv2, axis=1)
+
+    l = tf.nn.l2_normalize(rv1)
+    m = tf.nn.l2_normalize(rv2)
+
+    cos_half_c = tf.cos(a/2) * tf.cos(b/2) - (
+        tf.sin(a/2) * tf.sin(b/2) * tf.matmul(l, m, transpose_b=True))
+    sin_half_c_n = tf.sin(a/2) * tf.cos(b/2) * l + (
+        tf.cos(a/2) * tf.sin(b/2) * m + 
+        tf.sin(a/2) * tf.sin(b/2) * tf.cross(l, m))
+
+    half_c = tf.acos(cos_half_c)
+    n = sin_half_c_n / tf.sin(half_c)
+    return n * half_c * 2